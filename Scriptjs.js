4439981316d9ffa779e91bba1231078fbdec5e25
Catalin Mandru <112318980+mandru10@users.noreply.github.com>
2023-01-25 01:29:02 +0200

    Final Project Done

--- Scriptjs.js
+++ Scriptjs.js
@@ -60,4 +60,128 @@ moreInformationButton.onclick = (e) => {
     }
 }
 
-// 
+//Training buttons 
+
+const gymModal = document.getElementById("gymModal");
+const buttonGym = document.getElementById("buttonInfoGym");
+const buttonCloseGym = document.getElementById("modalCloseGym");
+const buttonCloseClasses = document.getElementById("modalCloseClasses");
+const buttonCloseBox = document.getElementById("modalCloseBox");
+const classesModal = document.getElementById("classesModal")
+const buttonClasses = document.getElementById("buttonInfoClasses");
+const boxModal = document.getElementById("boxModal");
+const buttonBox = document.getElementById("buttonInfoBox");
+
+
+buttonGym.addEventListener('click', openModal);
+buttonClasses.addEventListener('click', openModalClasses);
+buttonBox.addEventListener('click', openModalBox);
+buttonCloseGym.addEventListener("click", closeModalGym);
+buttonCloseClasses.addEventListener("click", closeModalClasses);
+buttonCloseBox.addEventListener("click", closeModalBox);
+    
+function openModal(){
+    gymModal.style.display = "block";
+
+}
+
+function openModalClasses(){
+    classesModal.style.display = "block";
+}
+
+function openModalBox(){
+    boxModal.style.display = "block";
+}
+
+function closeModalGym(){
+    gymModal.style.display = "none";
+}
+
+function closeModalClasses(){
+    classesModal.style.display = "none";
+}
+
+function closeModalBox(){
+    boxModal.style.display = "none";
+}
+
+//Order and buy buttons
+
+let cart = [];
+const buyButtons = document.getElementsByClassName('js-buy');
+
+
+Array.from(buyButtons).forEach(buyButton => {
+    buyButton.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const button = e.currentTarget;
+        const item = {
+            title: button.dataset.title,
+            price: button.dataset.price
+        }
+        const abonaments = ['silver', 'gold', 'platinum'];
+        const alreadyExist = cart.some(element => element.title === item.title && abonaments.includes(item.title));
+        
+        if(!alreadyExist){
+            cart.push(item)
+            renderCart();
+        }
+    })  
+})
+
+function renderCart(){
+    const shopContainer = document.getElementById("shopContainer");
+    
+    if(cart.length === 0){
+        shopContainer.classList.add("none");
+        return
+    }
+    const itemsHtml = cart.reduce((acc, item, index) => {return acc + `
+        <div class="item-cart"><button class="js-delete-item" data-id="${index}">x</button><span class="order-container">${item.title} - ${item.price}<span></div>
+    `}, '' + `<div id="message"><span id="orderPlaceholderName"></span></div>` + `<div class="submit"><button id="button-order">Send Order</button></div>` + 
+    `<div class="credentials"><input type="text" id="credentialsName" placeholder="Your Name" required minlength="1" maxlength="27" size="20"></div>`);
+    shopContainer.classList.remove("none");
+    shopContainer.innerHTML = itemsHtml;
+    setupListeners();
+
+}
+
+function setupListeners(){
+    const removeButtons = document.getElementsByClassName('js-delete-item');
+    const sendOrder = document.getElementById("button-order");
+    const showText = document.getElementById("orderPlaceholderName")
+    const credentials = document.getElementById("credentialsName");
+
+sendOrder.addEventListener('click', (eventSend) => {
+    eventSend.preventDefault();
+
+    if(credentials.value === "" || credentials.value == null){
+        showText.innerHTML = `Please enter your name !`;
+
+    }else{
+        message.style.display = "block";
+        showText.innerHTML = `&#9989; Order accepted ! Pick / Pay at GYM, <br> <span id="orderName">${credentials.value}</span>`;
+    }
+
+})
+
+    Array.from(removeButtons).forEach(removeButton => {
+        removeButton.addEventListener("click", (e) =>{
+            e.preventDefault();
+            const button = e.currentTarget;
+            const id = button.dataset.id;
+            const idInt = parseInt(id)
+            cart = removeElementAt(cart, idInt);
+            renderCart();
+        })
+    })
+}
+
+function removeElementAt(arr, index) {
+   let frontPart = arr.slice(0, index);
+   let lastPart  = arr.slice( index+1 );
+   return [...frontPart, ...lastPart];
+}
+
+
